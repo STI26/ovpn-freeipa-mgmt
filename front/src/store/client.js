e7100c4a6b1526e917a26b5bc22c4d1d831d5fdf
Siarhei Trus <s.trus@setplex.com>
2022-07-02 21:27:43 +0300

    added template for new actions

--- front/src/store/client.js
+++ front/src/store/client.js
@@ -43,6 +43,45 @@ export default {
         certificates
       }
       return data
+    },
+    async saveClient (context, data) {
+      return 'ok'
+    },
+    async createClient (context, data) {
+      return 'ok'
+    },
+    async deleteClient (context, data) {
+      return 'ok'
+    },
+    async revokeCert (context, data) {
+      return 'ok'
+    },
+    async downloadConfig (context, data) {
+      return 'ok'
+    },
+    async getUsers (context) {
+      const data = [
+        {id: 0, name: 'user 0'},
+        {id: 1, name: 'user 1'},
+        {id: 2, name: 'user 2'},
+        {id: 3, name: 'euser 3'},
+        {id: 4, name: 'user 4'},
+        {id: 11, name: 'user 11'},
+        {id: 12, name: 'j.dou'}
+      ]
+      return data
+    },
+    async getHosts (context) {
+      const data = [
+        {id: 0, name: 'host 0'},
+        {id: 1, name: 'host 1'},
+        {id: 2, name: 'host 2'},
+        {id: 3, name: 'ehost 3'},
+        {id: 4, name: 'host 4'},
+        {id: 11, name: 'host 11'},
+        {id: 12, name: 'localhost'}
+      ]
+      return data
     }
   }
 }
