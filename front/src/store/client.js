c3125a3d353defcb07183b45d123806620b91d99
Siarhei Trus <s.trus@setplex.com>
2022-08-03 00:59:04 +0300

    change mock to real

--- front/src/store/client.js
+++ front/src/store/client.js
@@ -13,75 +13,65 @@ export default {
     }
   },
   actions: {
-    async getClients (context) {
-      const data = [
-        {id: 0, name: 'client 0', numberOfCertificates: 1},
-        {id: 1, name: 'client 1', numberOfCertificates: 0},
-        {id: 2, name: 'client 2', numberOfCertificates: 2},
-        {id: 3, name: 'eclient 3', numberOfCertificates: 1},
-        {id: 4, name: 'client 4', numberOfCertificates: 3},
-        {id: 11, name: 'client 11', numberOfCertificates: 1},
-        {id: 12, name: 'j.dou', numberOfCertificates: 1}
-      ]
-      return data
+    async getClient ({ dispatch }, clientID='') {
+      return dispatch('fetch', {
+        path: '/users/'+clientID+'/config',
+        method: 'GET'
+      })
     },
-    async getClient (context, id) {
-      let certificates
-      if (id !== 3) {
-        certificates = [
-          {id: 12+id, revoked: false, issuedOn: 'Tue Jan 11 00:04:26 2022 UTC', expiresOn: 'Fri Jan 12 00:04:26 2024 UTC'},
-          {id: 14+id, revoked: true, issuedOn: 'Tue Jan 24 10:00:30 2020 UTC', expiresOn: 'Fri Feb 01 09:18:59 2030 UTC'}
-        ]
-      } else {
-        certificates = []
+    async saveClient ({ dispatch }, data) {
+      return dispatch('fetch', {
+        path: '/users/'+data.clientID+'/config/'+data.certificateID,
+        method: 'PUT',
+        body: data
+      })
+    },
+    async createClient ({ dispatch }, clientID) {
+      return dispatch('fetch', {
+        path: '/users/'+clientID+'/config',
+        method: 'POST'
+      })
+    },
+    async deleteClient ({ dispatch }, data) {
+      return dispatch('fetch', {
+        path: '/users/'+data.clientID+'/config/'+data.certificateID,
+        method: 'DELETE'
+      })
+    },
+    async revokeCert ({ dispatch }, data) {
+      return dispatch('fetch', {
+        path: '/certs/'+data.id,
+        method: 'DELETE'
+      })
+    },
+    async downloadConfig ({ dispatch }, data) {
+      return dispatch('download', {
+        path: '/users/'+data.clientID+'/config/'+data.certificateID,
+        method: 'GET'
+      })
+    },
+    async getUsers ({ dispatch }) {
+      return dispatch('fetch', {
+        path: '/users?all=true',
+        method: 'GET'
+      })
+    },
+    async getHosts ({ dispatch }) {
+      return dispatch('fetch', {
+        path: '/hosts',
+        method: 'GET'
+      })
+    },
+    async getCerts ({ dispatch }, username=null) {
+      let path = '/certs'
+      if (username !== null) {
+        path += '?subject=' + username
       }
-      const data = {
-        id: id,
-        subject: `client ${id}`,
-        ip: '10.10.2.10',
-        routes: 'push 192.168.0.0 dev\npush 192.168.1.0 dev',
-        certificates
-      }
-      return data
-    },
-    async saveClient (context, data) {
-      return 'ok'
-    },
-    async createClient (context, data) {
-      return 'ok'
-    },
-    async deleteClient (context, data) {
-      return 'ok'
-    },
-    async revokeCert (context, data) {
-      return 'ok'
-    },
-    async downloadConfig (context, data) {
-      return 'ok'
-    },
-    async getUsers (context) {
-      const data = [
-        {id: 0, name: 'user 0'},
-        {id: 1, name: 'user 1'},
-        {id: 2, name: 'user 2'},
-        {id: 3, name: 'euser 3'},
-        {id: 4, name: 'user 4'},
-        {id: 11, name: 'user 11'},
-        {id: 12, name: 'j.dou'}
-      ]
-      return data
-    },
-    async getHosts (context) {
-      const data = [
-        {id: 0, name: 'host 0'},
-        {id: 1, name: 'host 1'},
-        {id: 2, name: 'host 2'},
-        {id: 3, name: 'ehost 3'},
-        {id: 4, name: 'host 4'},
-        {id: 11, name: 'host 11'},
-        {id: 12, name: 'localhost'}
-      ]
-      return data
+
+      return dispatch('fetch', {
+        path: path,
+        method: 'GET'
+      })
     }
   }
 }
