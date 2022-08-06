79de690f0b0807e764d32863a6a03c92e4f7d7c0
Siarhei Trus <s.trus@setplex.com>
2022-08-06 04:17:03 +0300

    add search to config page

--- front/src/views/Config.vue
+++ front/src/views/Config.vue
@@ -1,10 +1,38 @@
 <script setup>
-import { onMounted, ref, watch, computed } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { useStore } from 'vuex'
 
 const store = useStore()
 
 const config = ref(null)
+
+const isInclude = (cfgItem, str) => {
+  const lstr = str.toLocaleLowerCase()
+  return (cfgItem.name.toLocaleLowerCase().includes(lstr) ||
+          cfgItem.value.toLocaleLowerCase().includes(lstr) ||
+          cfgItem.message.toLocaleLowerCase().includes(lstr))
+}
+
+const filteredConfig = computed(() => {
+  return store.getters.searchString === ''
+    ? config.value
+    : config.value.filter((item) => isInclude(item, store.getters.searchString))
+})
+
+const cfgToArray = (obj) => {
+  const list = []
+
+  for (const [key, val] of Object.entries(obj.value)) {
+    list.push({
+      name: key,
+      value: val,
+      status: false,
+      message: obj.status[key]
+    })
+  }
+  
+  return list
+} 
 
 const loadConfig = () => {
   store
@@ -14,7 +42,7 @@ const loadConfig = () => {
         throw data.error
       }
 
-      config.value = data.config
+      config.value = cfgToArray(data.config)
     })
     .catch((e) => {
       store.commit('updateToast', { color: 'danger', text: e })
@@ -37,12 +65,12 @@ onMounted(() => {
         </div>
       </div>
       <ol v-else class="list-group list-group-numbered">
-        <li v-for="[key, val] of Object.entries(config.value)" :key="key"
+        <li v-for="item of filteredConfig" :key="item.name"
           class="list-group-item d-flex justify-content-between align-items-start"
         >
           <div class="ms-2 me-auto">
-            <div class="fw-bold">{{ key }}:</div>
-            {{ val }} <span v-if="config.status[key]"> ({{ config.status[key] }})</span>
+            <div class="fw-bold">{{ item.name }}:</div>
+            {{ item.value }} <span v-if="item.message"> ({{ item.message }})</span>
           </div>
         </li>
       </ol>
