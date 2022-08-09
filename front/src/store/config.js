c404afe0055c53ddf162c974ad93a56d0a6b5efb
Siarhei Trus <s.trus@setplex.com>
2022-08-10 01:32:19 +0300

    add action updateCert, updateCA, updateCrl

--- front/src/store/config.js
+++ front/src/store/config.js
@@ -12,6 +12,24 @@ export default {
         method: 'POST',
         body: data
       })
+    },
+    async updateCert ({ dispatch }, id) {
+      return dispatch('fetch', {
+        path: '/config/cert/' + id,
+        method: 'POST'
+      })
+    },
+    async updateCA ({ dispatch }) {
+      return dispatch('fetch', {
+        path: '/config/ca',
+        method: 'POST'
+      })
+    },
+    async updateCrl ({ dispatch }) {
+      return dispatch('fetch', {
+        path: '/config/crl',
+        method: 'POST'
+      })
     }
   }
 }
