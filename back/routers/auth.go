2a882a78e7e73a542d574cd5e35877f93509f866
Siarhei Trus <s.trus@setplex.com>
2022-08-26 00:00:12 +0300

    add ValidateUser

--- back/routers/auth.go
+++ back/routers/auth.go
@@ -1,12 +1,51 @@
 package routers
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sti26/ovpn_freeipa_mgmt/controllers"
+	"github.com/sti26/ovpn_freeipa_mgmt/libs"
+	"github.com/sti26/ovpn_freeipa_mgmt/models"
 )
+
+func (r *Routers) ValidateUser(c *gin.Context, user, pass *string) (string, error) {
+	// Validate form input
+	if strings.Trim(*user, " ") == "" || strings.Trim(*pass, " ") == "" {
+		log.Println("[JSON_RPC] [Warn] Login/Password can't be empty")
+		return "", errors.New("empty login/password is not allowed")
+	}
+
+	token, err := r.Ipa.PostAuth(user, pass)
+	if err != nil {
+		return "", err
+	}
+
+	resp, _, err := r.Ipa.Jrpc(c, token, "user_find", []interface{}{}, map[string]interface{}{
+		"uid":      *user,
+		"in_group": *r.Cfg.IPAAllowGroup,
+	})
+	if err != nil {
+		log.Println("[JSON_RPC] [Warn] ", err)
+		return "", err
+	}
+
+	var obj models.RespObjUserFind
+	err = libs.ParseResponse(resp, &obj)
+	if err != nil {
+		log.Println("[JSON_RPC] [Error] ", err)
+		return "", err
+	}
+
+	if len(obj.Result) != 1 {
+		return "", fmt.Errorf("[JSON_RPC] [Warn] %s not found in %s group", *user, *r.Cfg.IPAAllowGroup)
+	}
+
+	return token, nil
+}
 
 func (r *Routers) AppLogin(c *gin.Context) {
 
@@ -18,7 +57,7 @@ func (r *Routers) AppLogin(c *gin.Context) {
 	var data creds
 	c.BindJSON(&data)
 
-	token, err := controllers.ValidateUser(c, r.Ipa, &data.Username, &data.Password, r.Cfg.IPAAllowGroup)
+	token, err := r.ValidateUser(c, &data.Username, &data.Password)
 	if err != nil {
 		log.Printf("[Log] Login failed: %s (%s)", data.Username, c.ClientIP())
 
