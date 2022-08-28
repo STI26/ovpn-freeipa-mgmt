d91f45288038379d8ef68282557d6fe935a990fd
Siarhei Trus <s.trus@setplex.com>
2022-08-28 16:20:06 +0300

    format doc

--- front/src/views/ClientConfigCreate.vue
+++ front/src/views/ClientConfigCreate.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { onMounted, ref, watch, computed } from 'vue';
-import { useRouter } from 'vue-router';
+import { onMounted, ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 
 const store = useStore()
@@ -11,16 +11,11 @@ const typeTarget = ref(null)
 const selectedTarget = ref(null)
 
 const spinner = ref(false)
-const spinnerClass = computed(() => {
-  return spinner.value
-    ? "spinner-border spinner-border-sm"
-    : ""
-})
 
 const loadOptions = (action) => {
   store
     .dispatch(action)
-    .then(data => {
+    .then((data) => {
       if (data.error !== '') {
         throw data.error
       }
@@ -32,8 +27,8 @@ const loadOptions = (action) => {
         selectedTarget.value = data[target][0].name
       }
     })
-    .catch(e => {
-      store.commit('updateToast', {color: 'danger', text: e})
+    .catch((e) => {
+      store.commit('updateToast', { color: 'danger', text: e })
       store.dispatch('showToast')
     })
 }
@@ -56,89 +51,100 @@ const onSubmit = () => {
     .then(() => {
       spinner.value = false
 
-      store.commit('updateToast', {color: 'success', text: 'Successful Create'})
+      store.commit('updateToast', {
+        color: 'success',
+        text: 'Successful Create'
+      })
       store.dispatch('showToast')
       router.push(next)
     })
-    .catch(e => {
+    .catch((e) => {
       spinner.value = false
-      store.commit('updateToast', {color: 'danger', text: e})
+      store.commit('updateToast', { color: 'danger', text: e })
       store.dispatch('showToast')
     })
 }
 
-watch(() => typeTarget.value, (newTarget, oldTarget) => {
-  loadOptions(newTarget)
-})
+watch(
+  () => typeTarget.value,
+  (newTarget, oldTarget) => {
+    loadOptions(newTarget)
+  }
+)
 
 onMounted(() => {
   typeTarget.value = 'getUsers'
   loadOptions(typeTarget.value)
-})
+});
 </script>
 
 <template>
-<div class="card mt-3">
-  <div class="card-header">
-    Select name
-  </div>
-  <div class="card-body">
-    <div class="row px-5">
-      <div class="col-9">
-        <select v-model="selectedTarget" class="form-select form-select mb-3" aria-label=".form-select">
-          <option v-for="opt in options" :key="opt.id" :value="opt.name">{{ opt.name }}</option>
-        </select>
+  <div class="card mt-3">
+    <div class="card-header">Select name</div>
+    <div class="card-body">
+      <div class="row px-5">
+        <div class="col-9">
+          <select
+            v-model="selectedTarget"
+            class="form-select form-select mb-3"
+            aria-label=".form-select"
+          >
+            <option v-for="opt in options" :key="opt.id" :value="opt.name">
+              {{ opt.name }}
+            </option>
+          </select>
+        </div>
+
+        <div class="col-3">
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              v-model="typeTarget"
+              type="radio"
+              value="getUsers"
+              name="flexRadioDefault"
+              id="flexRadioDefault1"
+              checked
+            />
+            <label class="form-check-label" for="flexRadioDefault1">
+              User
+            </label>
+          </div>
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              v-model="typeTarget"
+              type="radio"
+              value="getHosts"
+              name="flexRadioDefault"
+              id="flexRadioDefault2"
+            />
+            <label class="form-check-label" for="flexRadioDefault2">
+              Host
+            </label>
+          </div>
+        </div>
       </div>
 
-      <div class="col-3">
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            v-model="typeTarget"
-            type="radio"
-            value="getUsers"
-            name="flexRadioDefault"
-            id="flexRadioDefault1"
-            checked
-          />
-          <label class="form-check-label" for="flexRadioDefault1">
-            User
-          </label>
-        </div>
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            v-model="typeTarget"
-            type="radio"
-            value="getHosts"
-            name="flexRadioDefault"
-            id="flexRadioDefault2"
-          />
-          <label class="form-check-label" for="flexRadioDefault2">
-            Host
-          </label>
-        </div>
-      </div>
+      <router-link class="btn btn-secondary float-start" to="/"
+        >Cancel</router-link
+      >
+      <button
+        type="button"
+        class="btn btn-primary float-end"
+        @click="onSubmit"
+        :disabled="spinner"
+      >
+        <span
+          :class="spinner ? 'spinner-border spinner-border-sm' : ''"
+          role="status"
+          aria-hidden="true"
+        ></span>
+        <span v-if="spinner"> Create...</span>
+        <span v-else>Create</span>
+      </button>
     </div>
-
-    <router-link
-      class="btn btn-secondary float-start"
-      to="/"
-    >Cancel</router-link>
-    <button
-      type="button"
-      class="btn btn-primary float-end"
-      @click="onSubmit"
-      :disabled="spinner"
-    >
-    <span :class="spinnerClass" role="status" aria-hidden="true"></span>
-    <span v-if="spinner"> Create...</span>
-    <span v-else>Create</span>
-    </button>
   </div>
-</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
